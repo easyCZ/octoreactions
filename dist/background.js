36bd50f9312c6a29bce9f4c10599d97a08344279
Milan Pavlik <pavlik.mil@gmail.com>
2016-03-14 23:19:21 +0000

    Fix lastError

--- dist/background.js
+++ dist/background.js
@@ -9,15 +9,24 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   chrome.tabs.insertCSS(tabId, {
     file: 'css/octoreactions.css',
     runAt: 'document_start'
+  }, function () {
+    if (chrome.runtime.lastError) // value of `injected` above: don't inject twice
+      return;
   });
 
   chrome.tabs.executeScript(tabId, {
     file: 'lib/jquery-2.2.1.min.js',
     runAt: 'document_start'
   }, function () {
+    if (chrome.runtime.lastError) // value of `injected` above: don't inject twice
+      return;
+
     chrome.tabs.executeScript(tabId, {
       file: 'dist/octoreactions.js',
       runAt: 'document_start'
+    }, function () {
+      if (chrome.runtime.lastError) // value of `injected` above: don't inject twice
+        return;
     });
   });
 });
--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
   if (changeInfo.status !== 'complete') {
     return;
@@ -7,16 +7,24 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   chrome.tabs.insertCSS(tabId, {
     file: 'css/octoreactions.css',
     runAt: 'document_start'
+  }, function () {
+    if (chrome.runtime.lastError) // value of `injected` above: don't inject twice
+      return
   });
 
   chrome.tabs.executeScript(tabId, {
     file: 'lib/jquery-2.2.1.min.js',
     runAt: 'document_start'
-  }, () => {
+  }, function () {
+    if (chrome.runtime.lastError) // value of `injected` above: don't inject twice
+      return
+
     chrome.tabs.executeScript(tabId, {
       file: 'dist/octoreactions.js',
       runAt: 'document_start'
-    })
-  })
-
-})
+    }, function () {
+      if (chrome.runtime.lastError) // value of `injected` above: don't inject twice
+      return
+    });
+  });
+});
